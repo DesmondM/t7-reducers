df6d2b46da6ee1d22a9a83cc4302b680a287e90c
Desmond <desmondmpofu@gmail.com>
2023-09-15 18:04:23 +0200

    ammended

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import DateCounter from './DateCounter';
+import Counter from './Counter';
 export default function App() {
     return (
-        <div><DateCounter/></div>
+        <>
+        {/* <div><DateCounter/></div> */}
+        <div><Counter/></div>
+        </>
     );
 }
