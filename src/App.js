e6747f1b27ec7200998c55f6611464feac2f244d
Desmond <desmondmpofu@gmail.com>
2023-09-17 00:58:30 +0200

    ammended

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';
 import DateCounter from './DateCounter';
 import Counter from './Counter';
 import Header from './Header';
+import { Main } from './Main';
+
+const initialState   = {
+    questions: [],
+    //'loading', 'ready', 'error', 'active', 'finished'
+    status: 'loading',
+}
+const reducer = (state,action)=>{
+    switch(action.type){
+        case 'dataReceived' : return{
+            ...state,
+            questions: action.payload,
+            status: 'ready',
+        }
+       
+    }
+
+}
+
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    useEffect(() => {
+            fetch('http://localhost:8000/questions')
+            .then((res)=>res.json())
+            .then((data)=>dispatch({type:'dataReceived', payload:data}))
+    }, []);
     return (
         <div className='app'>
             <Header/>
-            <main className='main'>
+            <Main>
+                {state.status==='ready' && <p>Ready..</p>}
                 <p>1/15</p>
                 <p>Question</p>
-                </main>
+                </Main>
         {/* <div><DateCounter/></div> */}
         {/* <div><Counter/></div> */}
         </div>
